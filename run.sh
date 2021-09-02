687cdff0ddafecb5282cc76e062b1e79c67203a7
MD MAZHARUL ISLAM <mithu.cste.30@gmail.com>
2021-09-02 11:44:55 +0600

    update script to hold backup in dated folder

--- run.sh
+++ run.sh
@@ -6,8 +6,6 @@ redis_port=${REDIS_PORT:-"6379"}
 backup_type="redis"
 s3_bucket_name=${S3_BUCKET_NAME:-"binded-backups"}
 
-datestr=$(date +%Y-%m-%d)
-
 # just for testing...
 # redis-server > /dev/null &
 # while ! redis-cli ping; do sleep 1; done;
@@ -17,4 +15,4 @@ redis-cli \
   -p "$redis_port" \
   --rdb /tmp/dump.rdb
 
-aws s3 cp /tmp/dump.rdb "s3://${s3_bucket_name}/${backup_type}/redis-backup-${datestr}.rdb"
+aws s3 cp /tmp/dump.rdb "s3://${s3_bucket_name}/$(date +"%Y-%m-%d")/redis-backup-$(date +"T%H:%M:%SZ").rdb"
